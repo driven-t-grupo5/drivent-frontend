ba489f9378a3da2e5104ab9d2895f0aa5dabf12a
curtyraissa <raissacurty@gmail.com>
2023-08-02 01:02:55 +0100

    ao escolher o presencial exibe as opcoes

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -14,11 +14,28 @@ const objHospedagem = [
   { name: 'Com Hotel', price: 'R$350,00' },
 ];
 
+const HospedagemOptions = () => {
+  return (
+    <>
+      <Subtitle subtitle="Ótimo! Agora escolha sua modalidade de hospedagem" />
+      <StyledCard>
+        {objHospedagem.map((item, index) => (
+          <Card key={index} name={item.name} price={item.price} />
+        ))}
+      </StyledCard>
+    </>
+  );
+};
+
 export default function Payment() {
   const [showHospedagem, setShowHospedagem] = useState(false);
 
   const handlePresencialClick = () => {
-    setShowHospedagem(true);
+    setShowHospedagem((prev) => !prev);
+  };
+
+  const handleOnlineClick = () => {
+    // setShowHospedagem((prev) => !prev);
   };
 
   return (
@@ -27,20 +44,16 @@ export default function Payment() {
       <Subtitle subtitle="Primeiro, escolha sua modalidade de ingresso" />
       <StyledCard>
         {objCard.map((item, index) => (
-          <Card key={index} name={item.name} price={item.price} onClick={handlePresencialClick} />
+          <Card
+            key={index}
+            name={item.name}
+            price={item.price}
+            onClick={item.name === 'Presencial' ? handlePresencialClick : handleOnlineClick}
+          />
         ))}
       </StyledCard>
 
-      {showHospedagem && (
-        <>
-          <Subtitle subtitle="Ótimo! Agora escolha sua modalidade de hospedagem" />
-          <StyledCard>
-            {objHospedagem.map((item, index) => (
-              <Card key={index} name={item.name} price={item.price} />
-            ))}
-          </StyledCard>
-        </>
-      )}
+      {showHospedagem && <HospedagemOptions />}
     </>
   );
 }
