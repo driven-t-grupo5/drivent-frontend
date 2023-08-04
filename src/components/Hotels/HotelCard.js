7a07d6f621fa74123b74496f0b2daaa2d6a46d94
Davi Feliciano <dfeliciano37@gmail.com>
2023-08-04 14:58:02 -0300

    feat: listing room types in hotel cards

--- src/components/Hotels/HotelCard.js
+++ src/components/Hotels/HotelCard.js
@@ -1,5 +1,29 @@
 import { Card, CardContent, Typography, Grid, CardMedia, makeStyles, CardActionArea } from '@material-ui/core';
 
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function joinTypes(types) {
+  if (types.length === 1) {
+    return capitalize(types[0]);
+  }
+
+  if (types.length === 2) {
+    return `${capitalize(types[0])} e ${types[1]}`;
+  }
+
+  if (types.length === 3) {
+    return `${capitalize(types[0])}, ${types[1]} e ${types[2]}`;
+  }
+}
+
+function roomTypes(availableTypes) {
+  const possibleTypes = { 1: 'single', 2: 'double', 3: 'triple' };
+  const types = availableTypes.map((capacity) => possibleTypes[capacity]);
+  return joinTypes(types);
+}
+
 const useStyles = makeStyles({
   hotelCard: {
     width: 196,
@@ -11,7 +35,7 @@ const useStyles = makeStyles({
     marginTop: 16,
     borderRadius: 5,
   },
-  availableRooms: {
+  hotelSpec: {
     fontWeight: 700,
   },
 });
@@ -31,7 +55,13 @@ export default function HotelCard({ hotel, hotelSelected, setSelectedHotelId, se
           <CardMedia className={classes.media} image={hotel.image} />
           <CardContent>
             <Typography variant="h6">{hotel.name}</Typography>
-            <Typography className={classes.availableRooms} variant="caption">
+            <Typography className={classes.hotelSpec} variant="caption">
+              Tipos de acomodação:
+            </Typography>
+            <br />
+            <Typography variant="caption">{roomTypes(hotel.availableTypes)}</Typography>
+            <br />
+            <Typography className={classes.hotelSpec} variant="caption">
               Quartos disponíveis:
             </Typography>{' '}
             <Typography variant="caption">{hotel.availableRooms}</Typography>
